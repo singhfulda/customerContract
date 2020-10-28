63c93f111f37ad1cf2a719857e5d66453950384c
HarjeetSingh <singh.fulda@gmail.com>
2020-10-29 00:05:28 +0100

    endpoint implemented but not giveing back result still test is failing

--- customer/src/main/java/com/example/demo/CustomerController.java
+++ customer/src/main/java/com/example/demo/CustomerController.java
@@ -1,4 +1,15 @@
 package com.example.demo;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CustomerController {
+
+    @GetMapping("/customer/{id}")
+    private Customer getCustomer(@PathVariable("id") Long id) {
+        return null;
+    }
 }
