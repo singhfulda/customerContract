a76440850571ffa638950921d0895832b8988ccf
HarjeetSingh <singh.fulda@gmail.com>
2020-10-31 18:33:55 +0100

    update openapi

--- contract/src/main/java/com/example/demo/web/rest/PoliceApiDelegateImpl.java
+++ contract/src/main/java/com/example/demo/web/rest/PoliceApiDelegateImpl.java
@@ -6,21 +6,14 @@ import com.example.demo.service.PoliceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.context.request.NativeWebRequest;
 
 import java.math.BigDecimal;
-import java.util.Optional;
 
 @Service
 public class PoliceApiDelegateImpl implements PoliceApiDelegate {
 
     @Autowired
     PoliceService policeService;
-
-    @Override
-    public Optional<NativeWebRequest> getRequest() {
-        return Optional.empty();
-    }
 
     @Override
     public ResponseEntity<Void> createPolice(PoliceDTO body) {
